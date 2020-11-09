b5b4550ff63ac4a15c175d89ac6f153c4436c04f
mr-atharva-kulkarni <atharva.j.kulkarni1998@gmail.com>
2020-11-09 17:45:32 +0530

    uploaded modified ML-code

--- classifiers/model.py
+++ classifiers/model.py
@@ -10,7 +10,7 @@ import matplotlib.pyplot as plt
 def ml_classifier_model(model, x_train, x_val, y_train, y_val):
     model.fit(x_train, y_train)
     results = model.predict(x_val)
-    return model, accuracy_score(results, y_val), f1_score(results, y_val, average=None) 
+    return model, results
 
 
 # ------------------------------------------------------ CALLBACK CLASS --------------------------------------------
