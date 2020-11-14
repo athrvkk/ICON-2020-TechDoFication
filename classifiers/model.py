6a558f739f237ecd5138b6ab5daeae1752f83506
mr-atharva-kulkarni <atharva.j.kulkarni1998@gmail.com>
2020-11-14 23:29:20 +0530

    uploaded attention models

--- classifiers/model.py
+++ classifiers/model.py
@@ -1,4 +1,9 @@
+import numpy as np
+import pandas as pd
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
+from sklearn.metrics import confusion_matrix
+import itertools
+import matplotlib.pyplot as plt
 import tensorflow as tf
 from tensorflow.keras import Sequential
 from tensorflow.keras.models import load_model
@@ -11,6 +16,7 @@ def ml_classifier_model(model, x_train, x_val, y_train, y_val):
     model.fit(x_train, y_train)
     results = model.predict(x_val)
     return model, results
+
 
 
 # ------------------------------------------------------ CALLBACK CLASS --------------------------------------------
@@ -26,6 +32,7 @@ class myCallbacks(tf.keras.callbacks.Callback):
             self.model.stop_training = True
 
 
+            
 # ------------------------------------------------------ TRAIN DNN --------------------------------------------
 
 def create_model_DNN(input_dim, embedding_dim, embedding_matrix, pad_len, trainable=False, n1=64, n2=32, kr=None, br=None):
@@ -39,6 +46,7 @@ def create_model_DNN(input_dim, embedding_dim, embedding_matrix, pad_len, traina
     Simple.add(Dense(4, activation='softmax'))
     Simple.compile(optimizer='adam', loss="sparse_categorical_crossentropy", metrics=['acc'])
     return Simple
+
 
 
 # ------------------------------------------------------ TRAIN CNN --------------------------------------------
@@ -55,6 +63,7 @@ def create_model_CNN(input_dim, embedding_dim, embedding_matrix, pad_len, traina
     return myCNN
 
 
+
 # ------------------------------------------------------ TRAIN RNN --------------------------------------------
 
 def create_model_LSTM(input_dim, embedding_dim, embedding_matrix, pad_len, trainable, n1, n2, d=0.0):
@@ -68,6 +77,7 @@ def create_model_LSTM(input_dim, embedding_dim, embedding_matrix, pad_len, train
     myLSTM.add(Dense(4, activation='softmax'))
     myLSTM.compile(optimizer='adam', loss="sparse_categorical_crossentropy", metrics=['acc'])
     return myLSTM
+
 
 
 # ----------------------------------------------- PLOT ACCURACY LOSS GRAPH --------------------------------------------
@@ -92,6 +102,7 @@ def plot_curves(history):
     plt.show()
 
     
+    
 # ------------------------------------------------------ SAVE MODELS --------------------------------------------
 
 def save_model(model, name):
@@ -104,10 +115,12 @@ def save_model(model, name):
     model.save_weights("../models/"+name+"_weights.h5")
 
     
+    
 # ------------------------------------------------------ LOAD MODELS --------------------------------------------
 
 def model_load(path):
     return load_model(path)
+
 
 
 # ------------------------------------------------------ CLASSIFICATION REPORT --------------------------------------------
@@ -120,6 +133,45 @@ def classification_report(y_test, results):
     return acc, precision, recall, f1
 
 
+
+# ------------------------------------------------------ CONFUSION MATRIX --------------------------------------------
+
+def plot_confusion_matrix(y_val, 
+                          results,
+                          classes,
+                          normalize=False,
+                          title='Confusion matrix',
+                          cmap=plt.cm.Blues):
+    """
+    This function prints and plots the confusion matrix.
+    Normalization can be applied by setting `normalize=True`.
+    """
+    cm = confusion_matrix(y_val,results)
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    plt.colorbar()
+    tick_marks = np.arange(len(classes))
+    plt.xticks(tick_marks, classes, rotation=45)
+    plt.yticks(tick_marks, classes)
+
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+        print("Normalized confusion matrix")
+    else:
+        print('Confusion matrix, without normalization')
+
+    thresh = cm.max() / 2.
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        plt.text(j, i, cm[i, j],
+            horizontalalignment="center",
+            color="white" if cm[i, j] > thresh else "black")
+
+    plt.tight_layout()
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
+    
+    
+    
 # ------------------------------------------------------ PREDICT LABELS --------------------------------------------
 
 def predict_label(result):
